602efeb6e69b048f856d8f8dd8a701c306d16b13
hettipower <hettipower@gmail.com>
2019-11-27 17:52:03 +0530

    Add menu and style

--- src/components/contact-details/contact-details.component.jsx
+++ src/components/contact-details/contact-details.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectAddress , selectEmail , selectContactNo } from '../../redux/common/common.selectors';
+import { selectAddress , selectEmail , selectContactNo , selectMenus } from '../../redux/common/common.selectors';
 
 import './contact-details.styles.scss';
 
-const ContactDetails = ({ address , email , contactNo }) => (
+const ContactDetails = ({ address , email , contactNo , menus }) => (
     <div className="contactDetailsWrap" id="contact">
         <h2>Contact</h2>
         <div className="contactDetail">
@@ -21,13 +21,22 @@ const ContactDetails = ({ address , email , contactNo }) => (
             <h5>Mobile Phone</h5>
             {contactNo}
         </div>
+        <ul className="navbar-nav">
+            {
+                (menus)?
+                menus.map( menu => <li key={menu} className="nav-item"><a className="nav-link" href={menu.link}>{menu.name}</a></li>)
+                : ''
+            }                       
+        </ul>
+        
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
     address : selectAddress,
     email : selectEmail,
-    contactNo : selectContactNo
+    contactNo : selectContactNo , 
+    menus : selectMenus
 });
 
 export default connect(mapStateToProps)(ContactDetails);
